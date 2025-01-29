87e674e0addad8b53eaaffcbc8b080187d283a88
FAISAL\Faisal Kazi <faisalhkazi@gmail.com>
2025-01-29 10:12:42 +0530

    Pythoon-Study starts

---
+++ tuplee.py
@@ -0,0 +1,10 @@
+#Tuple is same like List but the difference is tuple does not support adding or modifying
+
+val = (1, 54, 98, 34, 7.88, "Faisal", 55.44, 884.34)
+
+print(val[4])
+
+val[5] = "FAISAL"
+
+print(val)
+

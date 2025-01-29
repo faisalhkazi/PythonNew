cdd5daa9f153f064aafa5a6049487516afc66dd3
FAISAL\Faisal Kazi <faisalhkazi@gmail.com>
2025-01-29 12:25:08 +0530

    Python-Study-Starts

---
+++ List.py
@@ -0,0 +1,6 @@
+#List can be modified del
+
+a = (77, 58, 96, 945, 66.54, 686, 65.77, "Faisal", 55.077, 78)
+
+print(a)
+
---
+++ Tuple.py
@@ -0,0 +1,11 @@
+#Tuple os sam like list but immutable cannot be modified
+
+a = (7858, 96, 945, 66.54, 686, 65.77, "Faisal", 55.077, 78)
+
+print(a)
+
+a[5] = "FAISAL"
+
+print(a[5])
+
+print(a)
